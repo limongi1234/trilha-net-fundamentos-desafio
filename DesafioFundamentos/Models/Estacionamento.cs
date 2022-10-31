2e8d14bb99d3209670734e580268cfd6309f4bb2
limongi1234 <93741519+limongi1234@users.noreply.github.com>
2022-10-31 19:42:35 -0300

    Update Estacionamento.cs

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -4,7 +4,7 @@ namespace DesafioFundamentos.Models
     {
         private decimal precoInicial = 0;
         private decimal precoPorHora = 0;
-        private List<string> veiculos = new List<string>();
+        private List <string> veiculos = new List <string>();
 
         public Estacionamento(decimal precoInicial, decimal precoPorHora)
         {
@@ -28,7 +28,7 @@ namespace DesafioFundamentos.Models
             string placa = "";
 
             // Verifica se o veículo existe
-            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            if (veiculos. Any(x => x.ToUpper() == placa.ToUpper()))
             {
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
 
@@ -44,24 +44,23 @@ namespace DesafioFundamentos.Models
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
             }
             else
-            {
                 Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
-            }
+            
         }
 
         public void ListarVeiculos()
         {
             // Verifica se há veículos no estacionamento
             if (veiculos.Any())
-            {
+            
                 Console.WriteLine("Os veículos estacionados são:");
                 // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
                 // *IMPLEMENTE AQUI*
-            }
+            
             else
-            {
+            
                 Console.WriteLine("Não há veículos estacionados.");
-            }
+            
         }
     }
 }
